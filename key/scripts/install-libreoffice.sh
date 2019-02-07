9802fb4ddd39ceaeeb8fc31a1a5d12bc97ddc75d
Andras Timar <andras.timar@collabora.com>
2019-02-07 10:12:30 +0100

    Update scripts for Collabora Online 4

--- key/scripts/install-libreoffice.sh
+++ key/scripts/install-libreoffice.sh
@@ -10,12 +10,12 @@ apt-get -y install apt-transport-https
 apt-get -y install locales-all
 
 # Add Collabora repos
-echo "deb https://collaboraoffice.com/${REPOS:-repos}/CollaboraOnline/3-key /" >> /etc/apt/sources.list.d/collabora.list
+echo "deb https://collaboraoffice.com/${REPOS:-repos}/CollaboraOnline/4-key /" >> /etc/apt/sources.list.d/collabora.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 6CCEA47B2281732DF5D504D00C54D189F4BA284D
 apt-get update
 
 # Install the Collabora packages
-apt-get -y install loolwsd collabora-online-brand collaboraoffice5.3-dict* collaboraofficebasis5.3*
+apt-get -y install loolwsd collabora-online-brand collaboraoffice6.0-dict* collaboraofficebasis6.0*
 
 # Install inotifywait and killall to automatic restart loolwsd, if loolwsd.xml changes
 apt-get -y install inotify-tools psmisc
